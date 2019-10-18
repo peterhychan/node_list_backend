090624f99c491ae4df697b2507da9042f9dfaf55
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2019-10-17 22:34:53 -0700

    connected to mongodb altas && changes command line commands colors

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express')
 const dotenv = require('dotenv')
 const logger = require('./middleware/logger')
 const morgan = require('morgan')
-
-// Route files
-const bootcamps = require('./routes/bootcamps')
+const colors = require('colors')
+const connectDB = require('./config/db')
 
 // Enable .env variables
 dotenv.config({ path: "./config/config.env"})
+
+// Connect to db
+connectDB()
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
 
 const app = express()
 
@@ -21,4 +26,12 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(process.env.NODE_ENV + " PORT: " + process.env.PORT))
+const server = app.listen(PORT, console.log(process.env.NODE_ENV.red.bold + " PORT: " + process.env.PORT.red.bold))
+
+// Handle unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+	console.log("Error".red + err.message)
+
+	//Close server
+	server.close(()=> process.exit(1))
+})
